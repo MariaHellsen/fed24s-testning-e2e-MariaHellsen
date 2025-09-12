1ab665f53fb0a5324e7c02853c12488137a06a87
MariaHellsen <maria.hellsen@outlook.com>
2025-09-12 12:21:49 +0200

    Fixed working test for srting button

--- cypress/e2e/sort.cy.ts
+++ cypress/e2e/sort.cy.ts
@@ -4,27 +4,29 @@ describe("Sorting function", () => {
   });
   it("should sort movies", () => {
     // Assign
-    const omdbInput = cy.get("input#searchText").should("exist");
-    const omdbSort = cy.get("button#sort").should("exist");
+
     cy.intercept("GET", "http://omdbapi.com/*", {
       statusCode: 200,
       fixture: "unsorted_movies.json",
     });
+    const omdbInput = cy.get("input#searchText").should("exist");
+    const omdbSort = cy.get("button#sort").should("exist");
 
     // Act
     omdbInput.type("Man{enter}");
+    cy.wait(500);
     omdbSort.click();
 
     // Assert
     cy.get("div#movie-container").children().should("have.length", 10);
     cy.get("div#movie-container").children().should("contain.text", "Man");
-    // cy.get("div#movie-container")
-    //   .children()
-    //   .first()
-    //   .should("contain.text", "Anchorman Man");
-    // cy.get("div#movie-container")
-    //   .children()
-    //   .last()
-    //   .should("contain.text", "Zoolander Man");
+    cy.get("div#movie-container")
+      .children()
+      .first()
+      .should("contain.text", "Anchorman Man");
+    cy.get("div#movie-container")
+      .children()
+      .last()
+      .should("contain.text", "Zoolander Man");
   });
 });
